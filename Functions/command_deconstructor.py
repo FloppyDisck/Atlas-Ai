bdc906d96a4455f57736de1afcc8492f215a5c5d
Guy S Garcia <guy.garcia@upr.edu>
2019-03-07 18:27:32 -0400

    Updated the file structure and fixed bugs

--- Functions/command_deconstructor.py
+++ Functions/command_deconstructor.py
@@ -9,7 +9,7 @@ from nltk.corpus import wordnet
 class CommandProcessing:
 
     def __init__(self, sentence, commands):
-  vs      self.sentence = sentence # Input sentence
+        self.sentence = sentence # Input sentence
         self.commands = commands # The dictionary input
         self.commandScore = {} # Initialize the score of the words
         self.extractedSentence = self.extract_NN() # Cleanup the sentence
@@ -153,85 +153,89 @@ if __name__ == "__main__":
 
     #Quick code testing with a big list
 
-    sentence_list = [
-        "is it going to rain?",
-        "how is the weather",
-        "what's today's forecast",
-        "Is today going to rain?",
-        "What is the weather in Chicago?",
-        "Is it hot or cold?",
-        "Is it sunny, should I take sunglasses?",
-        "Is it raining outside?",
-        "Should I take my umbrella?",
-        "What's the weather forecast?",
-        "What’s the weather expected to be tomorrow?",
-        "What’s the temperature?",
-        "How’s the weather?",
-        "What’s it like outside?",
-        "How's the weather?",
-        "Do you have rain?",
-        "What's the temperature in Manchester?",
-        "Is it a Beautiful day for a walk?"
-        "What's the weather forecast for the rest of the week?",
-        "what is the weather like tomorrow?",
-        "What will the weather be like tomorrow?",
-        "What is the weather going to be like tomorrow?",
-        "What is the weather forecast for tomorrow?",
-        "Are we gonna play a game today?",
-        "Do you wanna go out and eat?",
-        "Are you bored or just tired?"
-    ]
+    try:
+
+        sentence_list = [
+            "is it going to rain?",
+            "how is the weather",
+            "what's today's forecast",
+            "Is today going to rain?",
+            "What is the weather in Chicago?",
+            "Is it hot or cold?",
+            "Is it sunny, should I take sunglasses?",
+            "Is it raining outside?",
+            "Should I take my umbrella?",
+            "What's the weather forecast?",
+            "What’s the weather expected to be tomorrow?",
+            "What’s the temperature?",
+            "How’s the weather?",
+            "What’s it like outside?",
+            "How's the weather?",
+            "Do you have rain?",
+            "What's the temperature in Manchester?",
+            "Is it a Beautiful day for a walk?"
+            "What's the weather forecast for the rest of the week?",
+            "what is the weather like tomorrow?",
+            "What will the weather be like tomorrow?",
+            "What is the weather going to be like tomorrow?",
+            "What is the weather forecast for tomorrow?",
+            "Are we gonna play a game today?",
+            "Do you wanna go out and eat?",
+            "Are you bored or just tired?"
+        ]
 
 
-    import sqlite3
+        import sqlite3
 
-    #Dictionary Setup - Use the db to create the dictionary every bootup
-    conn = sqlite3.connect('commandDB')
-    db = conn.cursor()
-    db.execute("""
-    SELECT cs.StrName, c.StrName, c.StrType, c.IntIndex FROM commandsetuptbl cs
-            JOIN primarylisttbl p
-            JOIN functiontbl f
-            JOIN commandtbl c 
-    WHERE 
-            cs.IntPrimaryList_Id = p.ListId and
-            p.Function_Id = f.Id and
-            f.Id = c.FunctionId""")
+        #Dictionary Setup - Use the db to create the dictionary every bootup
+        conn = sqlite3.connect('commandDB')
+        db = conn.cursor()
+        db.execute("""
+        SELECT cs.StrName, c.StrName, c.StrType, c.IntIndex FROM commandsetuptbl cs
+                JOIN primarylisttbl p
+                JOIN functiontbl f
+                JOIN commandtbl c 
+        WHERE 
+                cs.IntPrimaryList_Id = p.ListId and
+                p.Function_Id = f.Id and
+                f.Id = c.FunctionId""")
 
-    commandDic = {}
+        commandDic = {}
 
-    for row in db.fetchall():
-            if (row[0] not in commandDic):
-                    commandDic[row[0]] = [[wordnet.synsets(str(row[1]), pos=str(row[2]))[int(row[3])]], {}]
-            else:
-                    commandDic[row[0]][0].append(wordnet.synsets(str(row[1]), pos=str(row[2]))[int(row[3])])
+        for row in db.fetchall():
+                if (row[0] not in commandDic):
+                        commandDic[row[0]] = [[wordnet.synsets(str(row[1]), pos=str(row[2]))[int(row[3])]], {}]
+                else:
+                        commandDic[row[0]][0].append(wordnet.synsets(str(row[1]), pos=str(row[2]))[int(row[3])])
 
-    db.execute("""
-    SELECT cs.StrName, c.StrName, c.StrType, c.IntIndex, f.StrFunction FROM commandsetuptbl cs
-            JOIN secondarylisttbl p
-            JOIN functiontbl f
-            JOIN commandtbl c 
-    WHERE 
-            cs.IntSecondaryList_Id = p.ListId and
-            p.Function_Id = f.Id and
-            f.Id = c.FunctionId""")
+        db.execute("""
+        SELECT cs.StrName, c.StrName, c.StrType, c.IntIndex, f.StrFunction FROM commandsetuptbl cs
+                JOIN secondarylisttbl p
+                JOIN functiontbl f
+                JOIN commandtbl c 
+        WHERE 
+                cs.IntSecondaryList_Id = p.ListId and
+                p.Function_Id = f.Id and
+                f.Id = c.FunctionId""")
 
-    for row in db.fetchall():
-            if (row[4] not in commandDic[row[0]][1]):
-                    commandDic[row[0]][1][row[4]] = [wordnet.synsets(str(row[1]), pos=str(row[2]))[int(row[3])]]
-            else:
-                    commandDic[row[0]][1][row[4]].append(wordnet.synsets(str(row[1]), pos=str(row[2]))[int(row[3])])
+        for row in db.fetchall():
+                if (row[4] not in commandDic[row[0]][1]):
+                        commandDic[row[0]][1][row[4]] = [wordnet.synsets(str(row[1]), pos=str(row[2]))[int(row[3])]]
+                else:
+                        commandDic[row[0]][1][row[4]].append(wordnet.synsets(str(row[1]), pos=str(row[2]))[int(row[3])])
 
-    for sentence in sentence_list:
+        for sentence in sentence_list:
 
-        command = CommandProcessing(sentence, commandDic)
+            command = CommandProcessing(sentence, commandDic)
 
-        command.primary_command_identifier() #First passthrough of the command
+            command.primary_command_identifier() #First passthrough of the command
 
-        print(command.secondary_command_identifier())
+            print(command.secondary_command_identifier())
 
-    #make this function work everything out and decide a similarity score out of the provided dictionaryaaa
-    #nltk.download('wordnet')
-    #nltk.download('punkt')
-    #nltk.download('averaged_perceptron_tagger')
-    #For Testing efficiency
+    except LookupError:
+
+        print("Performing first time setup of NLTK files")
+
+        nltk.download('wordnet')
+        nltk.download('punkt')
+        nltk.download('averaged_perceptron_tagger')
