c4cbe2e14ddcad019ff225c010c6a398e6019ed7
Guy S Garcia <guysebastiangarcia@gmail.com>
2019-03-15 10:10:27 -0400

    Further Progress on Weather API

--- Functions/open_weather_map_API.py
+++ Functions/open_weather_map_API.py
@@ -1,13 +1,22 @@
 #Open Weather Map API
 import os, json, requests
 
+#requestType = ["weather", "forecast"]
 
-apiKey = os.environ["OPEN_WEATHER_MAP_API_KEY"]
-requestSting = "https://api.openweathermap.org/data/2.5/weather?" + "q=London,u" + "k&appid=" + apiKey
-weatherData = requests.get(requestSting)
+def request_Weather(location, requestType): #if a date is requested ask forecast, if not weather
+    apiKey = os.environ["OPEN_WEATHER_MAP_API_KEY"]
+    location = "Puerto Rico"
+    requestSting = "https://api.openweathermap.org/data/2.5/" + requestType + "?" + "q=" + location + "&units=metric" + "&appid=" + apiKey 
+    weatherData = requests.get(requestSting)
+    #Make the string varie between dates and so forth
+    if (weatherData.status_code == requests.codes.ok):
+        #The request was completed without errors
+        weatherData = json.loads(weatherData.text)
+        returnString = '''Today you will be experiencing {}, the temperature is {} Celcius with a humidity of {} percent with a potential rain volume of {} milimiters.
+        '''.format(weatherData["weather"][0]["main"], weatherData["main"]["temp"], weatherData["main"]["humidity"], weatherData ["rain"]["3h"])
+        for key, keyData in weatherData.items():
+            print("{} : {}".format(key, keyData))
+        return returnString
 
-if (weatherData.status_code == requests.codes.ok):
-    #The request was completed without errors
-    weatherData = json.loads(weatherData.text)
-    for info in weatherData:
-        print(info)
+if __name__ == "__main__":
+    print(request_Weather("Puerto Rico", "weather"))
