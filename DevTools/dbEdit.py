1942770c0c93a7f33c44e1a61253f3ea15ca7020
FloppyDisck <guy.garcia@upr.edu>
2019-04-11 04:04:01 -0400

    Added data deleting support

--- DevTools/dbEdit.py
+++ DevTools/dbEdit.py
@@ -67,6 +67,8 @@ while notExit:
 
     if command[0] in ['show', 's']:
         db = conn.cursor() 
+        if command[1] in ['path', 'p']:
+            print(dbPath)
         if command[1] in ['data', 'd']: #show data in a raw form
             print('\nShowing all data in tables of {}'.format(dbName))
             db.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
@@ -164,7 +166,7 @@ while notExit:
                 #or c f 
                 for index in range(3, len(command)):
                     rawCommand = command[index]
-                    while True:
+                    while rawCommand != 'skip':
                         synCommand = str(rawCommand).split(".")
                         synCommand[1] = synCommand[1].lower()
                         if len(synCommand)  == 3 :
@@ -177,17 +179,12 @@ while notExit:
                                 commandSet = [synCommand[0], synCommand[1], synCommand[2], functionIndex]
 
                                 db.execute('INSERT INTO commandtbl (StrName, StrType, IntIndex, FunctionId) VALUES (?, ?, ?, ?)', commandSet)
-                                break #no errors found skipping
+                                rawCommand = 'skip' #no errors found skipping
 
-                            except:
-                                #TODO:NLTK did not find this value
-                                pass
-                            print('{} is not valid, expected "name.type.index"'.format(rawCommand))
-                            rawCommand = input('Please enter the correct value or type skip to ignore this value: ')
-
-                            if rawCommand == 'skip': #Forget the current option and keep going
-                                break
-
+                            except IndexError:
+                                print('{} is not a valid word found in the synset or expected "name.type.index" and received {}'.format(synCommand[0], rawCommand))
+                                rawCommand = input('Please enter the correct value or type skip to ignore this value: ')
+                                
         if command[1] in ['command', 'c']:
 
             #Check if the required ammout of arguments are provided
@@ -198,7 +195,6 @@ while notExit:
                 forContinue = True
                 listlist = ['primarylisttbl', 'secondarylisttbl']
                 for index in range(3, 5):
-                    print([listlist[index - 3]],[command[index]])
                     db.execute('SELECT * FROM {} l WHERE l.ListId = ?'.format(listlist[index - 3]), [command[index]])
                     if not db.fetchall():
                         forContinue = False
@@ -223,44 +219,93 @@ while notExit:
                 listlist = 'secondarylisttbl'
             else:
                 print('Received {} expected: c l [p, s] i n'.format(command))
+                continue
             
             try:
                 command[3] = int(command[3])
-            except:
+            except Exception as e:
                 print('{} is not valid, expected integer'.format(command[3]))
-                continue
-
-            for index in range(4, len(command)):
-                db.execute('SELECT * FROM functiontbl f WHERE f.Id = ?', [command[index]])
-                if not db.fetchall():
-                    print('Index {} not found in functiontbl, skipping.'.format(command[index]))
-                    continue
-                db.execute('SELECT * FROM {} l WHERE l.Function_Id = ? AND l.ListId = ?'.format(listlist), (command[3], command[index]))
-                if not db.fetchall():
-                    db.execute('INSERT INTO {} (Function_Id, ListId) VALUES (?, ?)'.format(listlist), (command[3], command[index]))
-                    print('Successfully added {}.'.format([command[3], command[index]]))
+            else:
+                for index in range(4, len(command)):
+                    db.execute('SELECT * FROM functiontbl f WHERE f.Id = ?', [command[index]])
+                    if not db.fetchall():
+                        print('Index {} not found in functiontbl, skipping.'.format(command[index]))
+                        continue
+                    db.execute('SELECT * FROM {} l WHERE l.ListId = ? AND l.Function_Id = ?'.format(listlist), (command[3], command[index]))
+                    if not db.fetchall():
+                        db.execute('INSERT INTO {} (ListId, Function_Id) VALUES (?, ?)'.format(listlist), (command[3], command[index]))
+                        print('Successfully added {}.'.format([command[3], command[index]]))
 
     if command[0] in ['delete', 'd']:
         db = conn.cursor() 
-        if command[1] in ['function', 'func', 'f']:
-            #TODO: if just delete function then also delete all synsets related to the function
-            #TODO: If more arguments the delete the given synsets
-            pass
+        if command[1] in ['function', 'func', 'f']: #delete functions and if allowed delete the individual commands
+            strFunction = functionStandard(command[2])
+            if len(command) == 3:
+                try:
+                    db.execute('DELETE FROM functiontbl f WHERE f.StrFunction = ?', [strFunction])
+                except Exception as e:
+                    print('{} could not be deleted, either it doesnt exist or an unexpected error happened!'.format(strCommand))
+                else:
+                    print('{} has been deleted!'.format(strCommand))
+            else:
+                for index in range(3, len(command)):
+                    synCommand = str(command[index]).split(".")
+                    synCommand[1] = synCommand[1].lower()
+                    try:
+                        commandSet = [synCommand[0], synCommand[1], synCommand[2], functionIndex]
+                        db.execute('SELECT * FROM functiontbl f WHERE f.StrFunction = ?', [strFunction])
+                        functionIndex = db.fetchone()[0]
+
+                        
+                        db.execute('DELETE FROM commandtbl c WHERE StrName = ? AND StrType = ? AND IntIndex = ? AND FunctionId = ?)', commandSet)
+
+                        print('{} found in {} was deleted successfully.'.format(command[index], strFunction))
+                    except Exception as e:
+                        print('{} found in {} was not deleted successfully.'.format(command[index], strFunction))
+
         if command[1] in ['command', 'c']:
-            #TODO: Delete the command
-            pass
-        if command[1] in ['list', 'l']:
-            #TODO: if just the first number delete the whole list and ask if you want to delete command
-            #TODO: else delete each in the list 
-            pass
+            strCommand = functionStandard(command[2])
+            try:
+                db.execute('DELETE FROM commandsetuptbl c WHERE c.StrName = ?', [strCommand])
+            except Exception as e:
+                print('{} could not be deleted, either it doesnt exist or an unexpected error happened!'.format(strCommand))
+            else:
+                print('{} has been deleted!'.format(strCommand))
+                
+        if command[1] in ['list', 'l']: 
+            if command[2] in ['primary', 'p']:
+                listlist = 'primarylisttbl'
+            elif command[2] in ['secondary', 's']:
+                listlist = 'secondarylisttbl'
+            else:
+                print('Received {} expected: c l [p, s] i n'.format(command))
+                continue
+            
+            if len(command) == 4:
+                try:
+                    db.execute('DELETE FROM {} l WHERE l.ListId = ?'.format(listlist), (command[3]))
+                except Exception as e:
+                    print('Could not delete {} list {}'.format(command[2], command[3]))
+                    print(e)
+                else:
+                    print('Deleted {} list {}'.format(command[2], command[3]))
+            else:
+                for index in range(4, len(command)):
+                    try:
+                        db.execute('DELETE FROM {} l WHERE l.ListId = ? AND l.Function_Id = ?'.format(listlist), (command[3], command[index]))
+                    except Exception as e:
+                        print('Could not delete {} list [{}, {}]'.format(command[2], command[3], command[index]))
+                        print(e)
+                    else:
+                        print('Deleted {} list [{}, {}]'.format(command[2], command[3], command[index]))
 
     if command[0] == 'commit':
         try:
             conn.commit()
             print('Changes saved.')
-
-        except:
+        except Exception as e:
             print('An unexpected error was found, settings were not commited.')
+            print(e)
 
     if command[0] in ['reset', 'r']:
         db.close()
