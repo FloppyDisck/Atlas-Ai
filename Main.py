d1890923b3f24e0ed7ab05ce132f3d28ebe7995f
FloppyDisck <guy.garcia@upr.edu>
2019-04-16 12:38:23 -0400

    Fixed minor issues

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from Functions import command_deconstructor
 from Functions import open_weather_map_API as weather
 
-sentence = "How is the weather like in 5 days in Madrid??"
+sentence = "How is the weather like in 5 days tomorrow in Madrid??"
 print(sentence)
 command = command_deconstructor.CommandProcessing()
 analizedSentence = command.analize_sentence(sentence)
@@ -12,12 +12,19 @@ print(analizedSentence)
 if (len(analizedSentence) > 0):
         #Continue
         if (list(analizedSentence.keys())[0] == "Weather"):
-                #from geotext import GeoText
-                #places = GeoText(sentence).cities
                 import geograpy
-                places = geograpy.get_place_context(text=sentence).names
+                places = geograpy.get_place_context(text=sentence)
 
-                print(places)
+                #TODO: add multivariable searching for 
+                # example: "Whats the weather, wind speed and rain volume for the last hour in Madrid"
+
+                if not places.regions:
+                        if not places.cities:
+                                location = places.countries[0]
+                        else:
+                                location = places.cities[0]
+                else:
+                        location = places.regions[0]
                 try:
                         print(weather.request_Weather(location, "weather"))
                 except:
