32abc016068113aaec4cc65262276c3c965e52d6
FloppyDisck <guy.garcia@upr.edu>
2019-04-30 17:03:31 -0400

    Testing the new reminder feature

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from Functions import command_deconstructor
 from Functions import open_weather_map_API as weather
+from Functions import reminders
 
 sentence = "How is the weather like in 5 days tomorrow in Madrid??"
 print(sentence)
@@ -11,13 +12,11 @@ print(analizedSentence)
 
 if (len(analizedSentence) > 0):
         #Continue
-        if (list(analizedSentence.keys())[0] == "Weather"):
+        mainCommand = list(analizedSentence.keys())[0]
+        if (mainCommand == "Weather"):
                 import geograpy
                 places = geograpy.get_place_context(text=sentence)
-
-                #TODO: add multivariable searching for 
-                # example: "Whats the weather, wind speed and rain volume for the last hour in Madrid"
-
+ 
                 if not places.regions:
                         if not places.cities:
                                 location = places.countries[0]
@@ -31,6 +30,13 @@ if (len(analizedSentence) > 0):
                         print("Location not found! Reverting to default.")
                         defaultLocation = "Puerto Rico"
                         print(weather.request_Weather(defaultLocation, "weather"))
+
+        if (mainCommand == "Reminder"):
+                from datetime import datetime, timezone
+                utc_dt = datetime.now(timezone.utc) # UTC time
+                dt = utc_dt.astimezone() # local time
+                print(utc_dt, dt)
+                print(utc_dt.strftime('%s'), dt.strftime('%s'))
 else:
         #Display error that command was not understood
         pass
