e76889f8475188fcc91acceae7d47a1148cf6126
FloppyDisck <guy.garcia@upr.edu>
2019-05-28 07:37:41 -0400

    Moved some things to module_manager.py

--- Main.py
+++ Main.py
@@ -1,44 +1,35 @@
-from Processes import command_deconstructor
-from Processes import open_weather_map_API as weather
-from Processes import reminders
+from Modules import module_manager
+from Modules import command_deconstructor
 
-sentence = "Weather ??"
+command = command_deconstructor.CommandProcessing("Modules/DataBases/commandDB")
+moduleManager = module_manager.Module_Manager()
+moduleManager.boot_manager()
+
+sentence = "What is the weather in Spain tomorrow"
 print(sentence)
-command = command_deconstructor.CommandProcessing("Processes/DataBases/commandDB")
+
+#TODO: 
+#If wifi enabled
+#       GetVPNServer.py runs to connect in a secure way
+
+#TODO: make this loop so incomplete calls can be completed with subsequent sentences
+#TODO: every call returns [status, sentence] status can mean call completed or call incomplete
+
 analizedSentence = command.analize_sentence(sentence)
 #Voice to text ends here
 
+#Returns a dictionary where the key is the command and the value is a list with 
+# 0.Dictionary with key = secondary command and value = word 
+# 1.Sentence list broken into lists where the second value is the secondary command
+#Example:
+#       In: What is the weather in Spain tomorrow
+#       Out: {'Weather': [{'Time': {'Tomorrow'}}, [['What'], ['is'], ['the'], ['weather'], ['in'], ['Spain'], ['tomorrow', 'Time']]]}
 print(analizedSentence)
 
 if (len(analizedSentence) > 0):
         #Continue
-        mainCommand = list(analizedSentence.keys())[0]
-        if (mainCommand == "Weather"):
-                import geograpy
-                places = geograpy.get_place_context(text=sentence)
-
-                try:
-
-                        if not places.regions:
-                                if not places.cities:
-                                        location = places.countries[0]
-                                else:
-                                        location = places.cities[0]
-                        else:
-                                location = places.regions[0]
-                        try:
-                                print(weather.request_Weather(location, "weather"))
-                        except:
-                                print("Location not found! Reverting to default.")
-                                defaultLocation = "Puerto Rico"
-                                print(weather.request_Weather(defaultLocation, "weather"))
-                except: 
-                        print("Location not found! Reverting to default.")
-                        defaultLocation = "Puerto Rico"
-                        print(weather.request_Weather(defaultLocation, "weather"))
-
-        if (mainCommand == "Reminder"):
-                pass
+        moduleManager.runtime_manager(analizedSentence, sentence)
+        
 else:
         #Display error that command was not understood
         pass
