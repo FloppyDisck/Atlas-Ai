c508660c9bf13605a96e2ceebfd165552297ae02
Guy S Garcia <guy.garcia@upr.edu>
2019-06-10 11:37:40 -0400

    modified for extended reminder testing

--- Main.py
+++ Main.py
@@ -5,10 +5,17 @@ command = command_deconstructor.CommandProcessing("Modules/DataBases/commandDB")
 moduleManager = module_manager.Module_Manager()
 moduleManager.boot_manager()
 
-sentence = "What is the weather in Spain tomorrow"
+#Remind me to clean up room at 7 30 pm tomorrow - reminder example
+#What is the weather in Spain tomorrow - weather example
+sentence = "Remind me to clean up room at 7 30 pm tomorrow"
+#Recognize its a reminder
+#regex to separate and get the reminder string "clean up room"
+#regex to get time
+#regex to get date
+#the concurrency starts at "every"
 print(sentence)
 
-#TODO: 
+#TODO:
 #If wifi enabled
 #       GetVPNServer.py runs to connect in a secure way
 
@@ -16,7 +23,10 @@ print(sentence)
 #TODO: every call returns [status, sentence] status can mean call completed or call incomplete
 
 analizedSentence = command.analize_sentence(sentence)
-analizedSentence_secondary = command.analize_sentence(sentence, True, 'Weather')
+print(analizedSentence)
+#analizedSentence_secondary = command.analize_sentence(sentence, True, 'Weather')
+#print(analizedSentence_secondary)
+
 #Voice to text ends here
 
 #Returns a dictionary where the key is the command and the value is a list with 
@@ -25,12 +35,12 @@ analizedSentence_secondary = command.analize_sentence(sentence, True, 'Weather')
 #Example:
 #       In: What is the weather in Spain tomorrow
 #       Out: {'Weather': [{'Time': {'Tomorrow'}}, [['What'], ['is'], ['the'], ['weather'], ['in'], ['Spain'], ['tomorrow', 'Time']]]}
-print(analizedSentence, analizedSentence_secondary)
 
 if (len(analizedSentence) > 0):
         #Continue
         pass
         #moduleManager.runtime_manager(analizedSentence, sentence)
+        #if the sentence is incomplete return a reply sentence and the primaryKey(commandKey)
         
 else:
         #Display error that command was not understood
