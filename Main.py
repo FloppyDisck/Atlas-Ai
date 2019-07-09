a0f228e31e56ea4eb0e30e45f3f1345877ed6a62
Guy Sebastian Garcia Pomales <34169809+FloppyDisck@users.noreply.github.com>
2019-07-09 12:45:19 -0400

    Updated the main

--- Main.py
+++ Main.py
@@ -1,47 +1,18 @@
-from Modules import module_manager
-from Modules import command_deconstructor
+#Starting the Bot
 
-command = command_deconstructor.CommandProcessing("Modules/DataBases/commandDB")
-moduleManager = module_manager.Module_Manager()
-moduleManager.boot_manager()
+from rasa_core.agent import Agent
+from rasa_core import utils
+from rasa_core.utils import EndpointConfig
 
-#Remind me to clean up room at 7 30 pm tomorrow - reminder example
-#What is the weather in Spain tomorrow - weather example
-sentence = "Remind me to clean up room at 7 30 pm tomorrow"
-#Recognize its a reminder
-#regex to separate and get the reminder string "clean up room"
-#regex to get time
-#regex to get date
-#the concurrency starts at "every"
-print(sentence)
+actionConfig = utils.read_yaml_file('configs/endpoints.yml')
+action_endpoint_url = actionConfig["action_endpoint"]["url"]
+agent = Agent.load('models/dialogue', interpreter='models/current/nlu', action_endpoint=EndpointConfig(url=action_endpoint_url))
 
-#TODO:
-#If wifi enabled
-#       GetVPNServer.py runs to connect in a secure way
-
-#TODO: make this loop so incomplete calls can be completed with subsequent sentences
-#TODO: every call returns [status, sentence] status can mean call completed or call incomplete
-
-analizedSentence = command.analize_sentence(sentence)
-print(analizedSentence)
-#analizedSentence_secondary = command.analize_sentence(sentence, True, 'Weather')
-#print(analizedSentence_secondary)
-
-#Voice to text ends here
-
-#Returns a dictionary where the key is the command and the value is a list with 
-# 0.Dictionary with key = secondary command and value = word 
-# 1.Sentence list broken into lists where the second value is the secondary command
-#Example:
-#       In: What is the weather in Spain tomorrow
-#       Out: {'Weather': [{'Time': {'Tomorrow'}}, [['What'], ['is'], ['the'], ['weather'], ['in'], ['Spain'], ['tomorrow', 'Time']]]}
-
-if (len(analizedSentence) > 0):
-        #Continue
-        pass
-        #moduleManager.runtime_manager(analizedSentence, sentence)
-        #if the sentence is incomplete return a reply sentence and the primaryKey(commandKey)
-        
-else:
-        #Display error that command was not understood
-        pass
+print("Testing the bot...")
+while True:
+    a = input()
+    if a == 'stop':
+        break
+    responses = agent.handle_message(a)
+    for response in responses:
+        print(response)
