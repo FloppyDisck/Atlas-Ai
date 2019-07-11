e15329c086e41394d57f65e8f5db446bed8efbbb
FloppyDisck <guy.garcia@upe.edu>
2019-07-11 19:19:14 -0400

    Updated File

--- Modules/enviromentVarSetup.py
+++ Modules/enviromentVarSetup.py
@@ -1,8 +1,18 @@
 import os
-
+os.system("source /etc/environment")
 variableName = ""
 variableValue = ""
 
-os.environ[variableName] = variableValue
-
-print(os.environ[variableName])
+while True:
+    if variableName == "":
+        variableName = input("Input the name of the variable: ")
+    else:
+        inputText = input("Input the value of the variable or write back to go back: ")
+        if inputText == "back" or "":
+            variableName = ""
+        else:
+            inputText = variableValue
+            os.environ[variableName] = str(variableValue)
+            print(f"{variableName} saved as {os.environ.get(variableName)}")
+            variableValue = ""
+            variableName = ""
