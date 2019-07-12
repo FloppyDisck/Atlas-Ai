6583cb1e0eef82a174cb622f04f43c569cc78c0c
Guy Garcia <guy.garcia@upr.edu>
2019-07-11 20:22:55 -0400

    Its now a file for testing

--- Modules/enviromentVarSetup.py
+++ Modules/enviromentVarSetup.py
@@ -1,18 +1,4 @@
 import os
 os.system("source /etc/environment")
-variableName = ""
-variableValue = ""
-
-while True:
-    if variableName == "":
-        variableName = input("Input the name of the variable: ")
-    else:
-        inputText = input("Input the value of the variable or write back to go back: ")
-        if inputText == "back" or "":
-            variableName = ""
-        else:
-            inputText = variableValue
-            os.environ[variableName] = str(variableValue)
-            print(f"{variableName} saved as {os.environ.get(variableName)}")
-            variableValue = ""
-            variableName = ""
+variableName = "OPEN_WEATHER_MAP_API_KEY"
+print(f"{variableName} read as {os.environ.get(variableName)}")
