34cb6e69a98d978ebba02388ee8929f8f50eecc8
FloppyDisck <guy.garcia@upr.edu>
2019-07-12 23:41:01 -0400

    Facilitated debugging process

--- atlas_train.py
+++ atlas_train.py
@@ -6,8 +6,10 @@ from rasa_nlu.model import Interpreter
 from rasa_nlu import config
 from rasa_nlu.test import run_evaluation
 
+#TODO: read json and add the nessesary datasets
+
 # loading the nlu training samples
-training_data = load_data("data/nlu.md")
+training_data = load_data("data/nlu/train/output.json")
 
 # trainer to educate our pipeline
 trainer = Trainer(config.load("configs/config.yml"))
@@ -23,27 +25,26 @@ model_directory = trainer.persist("./models", project_name='current', fixed_mode
 #More robust metrics report
 #run_evaluation("data/nlu.md", model_directory) #Recommended to have a custom test set
 
-#TODO: check if dividing my connectors (and, or) could solve the structured request problem
-#TODO: trained model not properly detecting location data
+dialogueTrain = False
+if dialogueTrain == True:
+    ##Train dialogue model
+    from rasa_core.policies import FallbackPolicy, KerasPolicy, MemoizationPolicy
+    from rasa_core.agent import Agent
 
-##Train dialogue model
-from rasa_core.policies import FallbackPolicy, KerasPolicy, MemoizationPolicy
-from rasa_core.agent import Agent
+    # this will catch predictions the model isn't very certain about
+    # there is a threshold for the NLU predictions as well as the action predictions
+    fallback = FallbackPolicy(fallback_action_name="utter_unclear",
+                            core_threshold=0.2,
+                            nlu_threshold=0.1)
 
-# this will catch predictions the model isn't very certain about
-# there is a threshold for the NLU predictions as well as the action predictions
-fallback = FallbackPolicy(fallback_action_name="utter_unclear",
-                          core_threshold=0.2,
-                          nlu_threshold=0.1)
+    agent = Agent('configs/domain.yml', policies=[MemoizationPolicy(), KerasPolicy(epochs=200, validation_split=0.0), fallback])
 
-agent = Agent('configs/domain.yml', policies=[MemoizationPolicy(), KerasPolicy(epochs=200, validation_split=0.0), fallback])
+    # loading our neatly defined training dialogues
+    training_data = agent.load_data('data/stories.md')
 
-# loading our neatly defined training dialogues
-training_data = agent.load_data('data/stories.md')
+    agent.train(training_data)
 
-agent.train(training_data)
-
-agent.persist('models/dialogue')
+    agent.persist('models/dialogue')
 
 #quick test to see if it was trained correctly
 def pprint(o): 
@@ -55,9 +56,9 @@ def pprint(o):
     Entities: {[i["value"] for i in o["entities"]]}''')
     #print(json.dumps(o, indent=2))
     
-pprint(interpreter.parse("would you be so kind to notify me at ten forty five to turn off lights"))
-pprint(interpreter.parse("would you be so kind to notify me at ten forty five"))
-pprint(interpreter.parse("set a notification for nine thirty"))
-pprint(interpreter.parse("Remind me to feed my gecko"))
+pprint(interpreter.parse("What is the weather like in new york"))
+pprint(interpreter.parse("Is it pouring outside?"))
+pprint(interpreter.parse("Is it chilly over in Hawaii?"))
+pprint(interpreter.parse("Is it chilly over in boston"))
 pprint(interpreter.parse("Set reminder for tomorrow at ten thirty pm"))
-pprint(interpreter.parse("can you set a reminder"))
+pprint(interpreter.parse("Is it going to be cold in the India?"))
