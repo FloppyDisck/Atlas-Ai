fde4d6b39bd49d0f3e117f3eb5867659d51c2d32
FloppyDisck <guy.garcia@upe.edu>
2019-08-21 21:41:39 -0400

    variable changes for conventions

--- custom_actions/action_reminder_set.py
+++ custom_actions/action_reminder_set.py
@@ -5,7 +5,7 @@ from dateutil.relativedelta import *
 from operator import itemgetter
 
 class CurrentReminders:
-    def __init__(self, dbPath = 'custom_actions/storage/remindersDB'):
+    def __init__(self, db_path = 'custom_actions/storage/remindersDB'):
         """
         Create a reminder monitor object.
 
@@ -13,12 +13,12 @@ class CurrentReminders:
         theyre due.
 
         Args:
-            dbPath: Where to get the reminders from.
+            db_path: Where to get the reminders from.
         """
-        self.conn = sqlite3.connect(dbPath)
+        self.conn = sqlite3.connect(db_path)
         self.update_list()
 
-    def set_reminder(self, reminder, alarm_epoch, repeatFrequency = None):
+    def set_reminder(self, reminder, alarm_epoch, repeat_frequency = None):
         """
         Create a reminder and add it to the reminder DB
 
@@ -26,33 +26,33 @@ class CurrentReminders:
             reminder: The reminder description.
             alarm: Array list that will be added to time.
             exactTime: A boolean that describes the alarm type.
-            repeatFrequency: A string that describes the repetition type.
+            repeat_frequency: A string that describes the repetition type.
                 Repeat every:
-                s[second]/m[minutes]/h[hour]/D[day]/M[month]/Y[year]/T[times]/w[WeekDay]/W[Weeks]
+                s[second]/m[minutes]/h[hour]/D[day]/M[month]/Y[year]/T[times]/w[week_day]/W[Weeks]
 
         Returns:
             Returns info that confirms the saved data.
         """
 
-        if repeatFrequency == None:
-            reminderStatus = 1
+        if repeat_frequency == None:
+            reminder_status = 1
         else:
             #Run reminder_concurrency after alarm is done
-            reminderStatus = 2
+            reminder_status = 2
 
         #Add the reminder to the ram
-        commandSet = [reminder, alarm_epoch, reminderStatus, repeatFrequency]
-        self.reminders.append(commandSet)
+        command_set = [reminder, alarm_epoch, reminder_status, repeat_frequency]
+        self.reminders.append(command_set)
 
         #Sort by the smallest epoch
         self.reminders.sort(key = itemgetter(1))
 
         db = self.conn.cursor()
-        db.execute('INSERT INTO reminderstbl (reminderStr, alarm, status, concurrency) VALUES (?, ?, ?, ?)', commandSet)
+        db.execute('INSERT INTO reminderstbl (reminderStr, alarm, status, concurrency) VALUES (?, ?, ?, ?)', command_set)
         self.conn.commit()
 
         #return string confirmation of "set blah blah at TIMEHERE"
-        return commandSet
+        return command_set
 
     def delete_reminder(self, reminder):
         try:
@@ -75,32 +75,32 @@ class CurrentReminders:
         WHERE r.status > 0 AND r.alarm >= ? AND r.alarm < ?""", (epoch_day, epoch_day + 86400))
         self.conn.commit()
         #If status is greater than 0 then it is still available
-        reminderList = []
+        reminder_list = []
         
         for reminder in db.fetchall():
-            reminderList.append(reminder)
+            reminder_list.append(reminder)
 
         #sort by epoch
-        reminderList.sort(key = itemgetter(1))
+        reminder_list.sort(key = itemgetter(1))
 
         #if a day in the close week say so
         #You have reminders tomorrow; you have reminders this sunday
-        dayDate = datetime.fromtimestamp(epoch_day).strftime('%m %d') 
-        if len(reminderList) == 0:
-            returnString = f"You have no reminders for {dayDate}"
-        elif len(reminderList) == 1:
-            returnString = f"You only have to {reminderList[0][0]} at {datetime.fromtimestamp(reminderList[0][1]).strftime('%H %M')}"
-        elif len(reminderList) == 2:
-            returnString = f"You have to {reminderList[0][0]} at {datetime.fromtimestamp(reminderList[0][1]).strftime('%H %M')} and then {reminderList[1][0]} at {datetime.fromtimestamp(reminderList[1][1]).strftime('%H %M')}"
-        elif len(reminderList) > 2:
+        day_date = datetime.fromtimestamp(epoch_day).strftime('%m %d') 
+        if len(reminder_list) == 0:
+            return_string = f"You have no reminders for {day_date}"
+        elif len(reminder_list) == 1:
+            return_string = f"You only have to {reminder_list[0][0]} at {datetime.fromtimestamp(reminder_list[0][1]).strftime('%H %M')}"
+        elif len(reminder_list) == 2:
+            return_string = f"You have to {reminder_list[0][0]} at {datetime.fromtimestamp(reminder_list[0][1]).strftime('%H %M')} and then {reminder_list[1][0]} at {datetime.fromtimestamp(reminder_list[1][1]).strftime('%H %M')}"
+        elif len(reminder_list) > 2:
             import random
-            flavorStrings = ["then ", "later ", "after ", "and ", "also "]
-            returnString = f"You have to {reminderList[0][0]} at {datetime.fromtimestamp(reminderList[0][1]).strftime('%H %M')} "
-            for index in range(1, len(reminderList) - 1):
-                randIndex = random.randint(0, len(flavorStrings) - 1)
-                returnString = returnString + flavorStrings[randIndex] + f"{reminderList[index][0]} at {datetime.fromtimestamp(reminderList[index][1]).strftime('%H %M')} "
-            returnString = returnString + f"finally remember to {reminderList[-1][0]} at {datetime.fromtimestamp(reminderList[-1][1]).strftime('%H %M')}"
-        return returnString
+            flavor_strings = ["then ", "later ", "after ", "and ", "also "]
+            return_string = f"You have to {reminder_list[0][0]} at {datetime.fromtimestamp(reminder_list[0][1]).strftime('%H %M')} "
+            for index in range(1, len(reminder_list) - 1):
+                rand_index = random.randint(0, len(flavor_strings) - 1)
+                return_string = return_string + flavor_strings[rand_index] + f"{reminder_list[index][0]} at {datetime.fromtimestamp(reminder_list[index][1]).strftime('%H %M')} "
+            return_string = return_string + f"finally remember to {reminder_list[-1][0]} at {datetime.fromtimestamp(reminder_list[-1][1]).strftime('%H %M')}"
+        return return_string
     
     def reminder_concurrency(self, reminder_old):
         """
@@ -114,99 +114,98 @@ class CurrentReminders:
         """
         #second, minute, hour, Day, Month, Year, Times, Weeks, week (week day 1-7) (days divided by -)
         repetition = reminder_old[3].split('/')
-        newAlarm = datetime.fromtimestamp(reminder_old[1])
+        new_alarm = datetime.fromtimestamp(reminder_old[1])
 
         repetition_str = ""
         for value in repetition:
             if value != "":
                 #Set the header Char
-                valueHeader = value[:1]
+                value_header = value[:1]
 
                 #Relative delta handles leapyears and month disbalances
-                if valueHeader == 'Y':
-                    newAlarm += relativedelta(years=int(value[1:]))
-                if valueHeader == 'M':
-                    newAlarm += relativedelta(months=int(value[1:]))
-                if valueHeader == 'D':
-                    newAlarm += relativedelta(days=int(value[1:]))
-                if valueHeader == 'h':
-                    newAlarm += relativedelta(hours=int(value[1:]))
-                if valueHeader == 'm':
-                    newAlarm += relativedelta(minutes=int(value[1:]))
-                if valueHeader == 's':
-                    newAlarm += relativedelta(seconds=int(value[1:]))
-                if valueHeader == 's':
-                    newAlarm += relativedelta(weeks=int(value[1:]))
+                if value_header == 'Y':
+                    new_alarm += relativedelta(years=int(value[1:]))
+                if value_header == 'M':
+                    new_alarm += relativedelta(months=int(value[1:]))
+                if value_header == 'D':
+                    new_alarm += relativedelta(days=int(value[1:]))
+                if value_header == 'h':
+                    new_alarm += relativedelta(hours=int(value[1:]))
+                if value_header == 'm':
+                    new_alarm += relativedelta(minutes=int(value[1:]))
+                if value_header == 's':
+                    new_alarm += relativedelta(seconds=int(value[1:]))
+                if value_header == 's':
+                    new_alarm += relativedelta(weeks=int(value[1:]))
 
                 #Remove a repetition since this is finite
-                if valueHeader == 'T':
+                if value_header == 'T':
                     times = int(value[1:])
                     if times == 0:
                         return None
                     value = 'T' + str(times-1)
 
-                if valueHeader == 'w':
+                if value_header == 'w':
                     #Assuming in this string weeks start with 1
-                    nextDay = 0
-                    weekDay_now = datetime.now(timezone.utc).weekday() + 1
-                    weekDays = sorted(value[1:].split('-'))
+                    next_day = 0
+                    week_day_now = datetime.now(timezone.utc).week_day() + 1
+                    week_days = sorted(value[1:].split('-'))
 
-                    #Check which weekDay is the next one
-                    for weekDay in weekDays:
-                        if weekDay_now <= weekDay:
-                            nextDay = weekDay
-                    if nextDay == 0:
-                        nextDay = weekDays[0]
+                    #Check which week_day is the next one
+                    for week_day in week_days:
+                        if week_day_now <= week_day:
+                            next_day = week_day
+                    if next_day == 0:
+                        next_day = week_days[0]
 
                     #Calculate how many days to skip
-                    if weekDay_now == nextDay:
-                        newAlarm += relativedelta(days=7)
-                    elif weekDay_now > nextDay:
-                        newAlarm += relativedelta(days=(7 - weekDay_now + nextDay))
+                    if week_day_now == next_day:
+                        new_alarm += relativedelta(days=7)
+                    elif week_day_now > next_day:
+                        new_alarm += relativedelta(days=(7 - week_day_now + next_day))
                     else:
-                        newAlarm += relativedelta(days=(nextDay - weekDay_now))
+                        new_alarm += relativedelta(days=(next_day - week_day_now))
  
                 repetition_str += '/' + value
 
-        newReminder = self.set_reminder(reminder_old[0], newAlarm, repeatFrequency=repetition_str)
-        return newReminder
+        new_reminder = self.set_reminder(reminder_old[0], new_alarm, repeat_frequency=repetition_str)
+        return new_reminder
 
-    def check_alarm(self):
+    def check_alarm(self, custom_time = None):
         """
         Go through self.reminders and check what alarm is ready.
 
         Args:
-            self.reminders: List of all the reminders in a threshold.
+            self.reminders: List of all the reminders.
+            custom_time: Replaces curren_time_epoch which is current seconds since epoch
 
         Returns:
             Return list of alarms that are due.
         """
-        threshold = 0 #add second threshold for current time
-        currentTime_epoch = datetime.now(timezone.utc).timestamp() + threshold
-        dueAlarm = []
+        current_time_epoch = datetime.now(timezone.utc).timestamp()
+        due_alarm = []
         for reminder in self.reminders:
             #Since list is sorted break when number is greater
-            if currentTime_epoch < reminder[1]:
-                break
-            #Add reminder to dueAlarm and remove from reminders
-            dueAlarm.append(reminder)
-            if reminder[2] == 2: #Standard
-                self.reminder_concurrency(reminder)
+            if current_time_epoch >= reminder[1]:
+                #Add reminder to due_alarm and remove from reminders
+                due_alarm.append(reminder)
+                if reminder[2] == 2: #Standard
+                    self.reminder_concurrency(reminder)
  
-            db = self.conn.cursor()
-            db.execute('UPDATE remindersTbl SET status = ? WHERE reminderStr = ? AND alarm = ?', (0, reminder[0], reminder[1]))
-            self.reminders.remove(reminder)
-            self.conn.commit()
+                db = self.conn.cursor()
+                db.execute('UPDATE remindersTbl SET status = ? WHERE reminderStr = ? AND alarm = ?', (0, reminder[0], reminder[1]))
+                self.reminders.remove(reminder)
+                self.conn.commit()
 
         #TODO: create a string for each value that the ai can read
-        return dueAlarm
+        return due_alarm
 
     def update_list(self):
         """
         Update the self.reminders from the database.
 
         Returns:
-            self.reminders witt the populated reminders.
+            self.reminders with the populated reminders.
         """
         #timeUTC_epoch = datetime.now(timezone.utc).timestamp()
         db = self.conn.cursor()
@@ -222,6 +221,19 @@ class CurrentReminders:
 
         #sort by epoch
         self.reminders.sort(key = itemgetter(1))
+    
+    def clear_list(self):
+        """
+        Erase all database contents.
+
+        Returns: 
+            nothing
+        """
+
+        db = self.conn.cursor()
+        db.execute("DELETE FROM remindersTbl")
+        self.conn.commit()
+        self.update_list()
 
 if __name__ == "__main__":
     reminder = CurrentReminders()
