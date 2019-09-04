8d4be55ca2d855c92d53ff078770343363b97480
FloppyDisck <guy.garcia@upe.edu>
2019-09-04 13:49:35 -0400

    Variable naming changes

--- custom_actions/action_weather_get.py
+++ custom_actions/action_weather_get.py
@@ -7,10 +7,10 @@ class Weather():
         self.api_key = api_key
     def get_weather(self):
         import json, requests
-        requestSting = f"https://api.openweathermap.org/data/2.5/{self.request_type}?q={self.location}&units={self.temp_unit}&appid={self.api_key}"
-        weatherData = requests.get(requestSting)
-        if (weatherData.status_code == requests.codes.ok):#If the request is good
-            weatherData = json.loads(weatherData.text)
-            return weatherData
+        request_string = f"https://api.openweathermap.org/data/2.5/{self.request_type}?q={self.location}&units={self.temp_unit}&appid={self.api_key}"
+        weather_data = requests.get(request_string)
+        if (weather_data.status_code == requests.codes.ok):#If the request is good
+            weather_data = json.loads(weather_data.text)
+            return weather_data
         else:
             return None
