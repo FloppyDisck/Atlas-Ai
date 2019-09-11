1fb76563db62818d09e217125bb3ded80868598b
Guy S Garcia <guy.garcia@upr.edu>
2019-09-11 04:19:59 -0400

    Small logic fix

--- actions.py
+++ actions.py
@@ -1,6 +1,8 @@
 # in duckling files stack exec duckling-example-exe
 # Enviroment vars are setup in ~/.profile, export varName=varValue
 
+#trackers = tracker.current_slot_values()
+
 from rasa_sdk import Action
 from rasa_sdk.events import SlotSet
 import os
@@ -25,26 +27,26 @@ class ActionWeatherReturn(Action):
     def run(self, dispatcher, tracker, domain):
         from custom_actions.action_weather_get import Weather 
         #TODO: Make module that generates a log file with error
-
-        trackers = tracker.current_slot_values()
-        if tracker.get_slot('location') == None:
-            location = "Puerto Rico"
-        else:
-            location = tracker.get_slot('location')
-        
         #Keep enclosed to reduce random errors
         try:
             api_key = get_var("OPEN_WEATHER_MAP_API_KEY")
-            temp_unit = "Celcius" #Temp unit might change depending request
 
-        # Get location
-        if tracker.get_slot('location') != None:
-            weatherReport.location = tracker.get_slot('location')
-            
-            request_type = "forecast"
+            weatherReport = Weather(api_key)
+            weather_arg = tracker.get_slot('weather_arg')
 
-            #Ugly try catch for checking which format added
-            time_string = "today"
+            # Change temperature units
+            if "metric" in weather_arg:
+                weatherReport.temp_unit = "Celcius"
+            elif "imperial" in weather_arg:
+                weatherReport.temp_unit = "Fahrenheit"
+
+            # Change location
+            if tracker.get_slot('location') == None:
+                weatherReport.location = "Puerto Rico"
+            else:
+                weatherReport.location = tracker.get_slot('location')
+
+            # Ugly try catch for checking which format added
             try:
                 time_command = tracker.get_slot('time')[0][:-6]
                 threshold = 0
@@ -55,7 +57,9 @@ class ActionWeatherReturn(Action):
                 except TypeError:
                     time_command = None
 
+            # Check what day were refering too
             time_difference = 0
+            time_string = "today"
             if time_command is not None:  # If last part ran successfully
                 from datetime import datetime, timezone
                 #Compare current time with requested time
@@ -70,72 +74,60 @@ class ActionWeatherReturn(Action):
                     elif (time_difference > 1) and (time_difference <= 5):
                         time_string = f"in {time_difference} days"
                 else:
-                    request_type = "error"
-                
-            except Exception as e:
-                time_difference = 0
-                print(e)
-
-            if request_type == "error":
-                #If requesting wrong date
-                print("User input time not allowed in api")
-                dispatcher.utter_message("The A P I allowed forecast time is from today to the next 5 days.")
-
+                    print(f"time_command is {time_command}; greater/lower than the allowed parameters")
+                    if time_command > 5:
+                        time_command = 5
+                    elif time_command < 0:
+                        time_command = 0
             else:
-                #Build string and get the slot values
-                request_string = f"https://api.openweathermap.org/data/2.5/{request_type}?q={location}&units={temp_unit}&appid={api_key}"
-                weather_data = requests.get(request_string)
-                weather_arg = tracker.get_slot('weather_arg') #gets the slot value
-                trackers = tracker.current_slot_values()
-                print("Weather Args: ", weather_arg)
-                print("trackers ", trackers)
+                print("time_command is None; defaulting to today's weather")
+                time_difference = 0
 
-                if (weather_data.status_code == requests.codes.ok):#If the request is good
-                    weather_data = json.loads(weather_data.text)
+            # Get weather
+            weather_data = weatherReport.get_weather()
+            weather_data = weather_data['list'][time_difference]
                     
-                    #Response dictionary build
-                    return_dic = {"default":"", "rain":"", "quantity":"", "temp":"", "humidity":"", "clowdy":""}
-                    if request_type == "forecast":
-                        weather_data = weather_data['list'][time_difference]
-                    return_dic['default'] = f"You will be experiencing {weather_data['weather'][0]['description'].lower()}"
-                    if weather_arg is not None:
-                        for arg in weather_arg:
-                            if arg == "rain":
-                                try:
-                                    return_dic[arg] = f"up to {weather_data['rain']['1h']} milileters of rain"
-                                    if "quantity" not in weather_arg:
-                                        return_dic[arg] = "rain"
-                                except:
-                                    return_dic[arg] = "no rain"
-                            if arg == "temp":
-                                if "metric" in weather_arg:
-                                    temp_unit = "Celcius"
-                                elif "imperial" in weather_arg:
-                                    temp_unit = "Fahrenheit"
-                                return_dic[arg] = f"temperatures of {weather_data['main']['temp']} {temp_unit}"
-                            if arg == "wind":
-                                return_dic[arg] = f"a wind speed of {weather_data['wind']['speed']} miles per hour"
-                            if arg == "humidity":
-                                return_dic[arg] = f"a humidity of {weather_data['main']['humidity']} percent" #TODO: humidity verbal descriptors
-                            if arg == "clowdy":
-                                return_dic[arg] = f"a clowdiness of {weather_data['clouds']['all']} percent" #TODO: humidity verbal descriptors111
-                            if arg == "snowing":
-                                try:
-                                    return_dic[arg] = f"up to {weather_data['snow']['1h']} milileters of rain"
-                                    if "quantity" not in weather_arg:
-                                        return_dic[arg] = "snow"
-                                except:
-                                    return_dic[arg] = "no snow"
-                            
+            # Response dictionary build
+            return_dic = {"default":"", "connector":"", "rain":"", "quantity":"", "temp":"", "humidity":"", "clowdy":""}
+            return_dic['default'] = f"You will be experiencing {weather_data['weather'][0]['description'].lower()}"
+
+            # Build the responce sentence
+            if weather_arg is not None:
+                return_dic[connector] = "with"
+                for arg in weather_arg:
+                    if arg == "rain":
+                        try:
+                            return_dic[arg] = f"up to {weather_data['rain']['1h']} milileters of rain"
+                            if "quantity" not in weather_arg:
+                                return_dic[arg] = "rain"
+                        except:
+                            return_dic[arg] = "no rain"
+                    if arg == "temp":
+                        return_dic[arg] = f"temperatures of {weather_data['main']['temp']} {temp_unit}"
+                    if arg == "wind":
+                        return_dic[arg] = f"a wind speed of {weather_data['wind']['speed']} miles per hour"
+                    if arg == "humidity":
+                        return_dic[arg] = f"a humidity of {weather_data['main']['humidity']} percent" #TODO: humidity verbal descriptors
+                    if arg == "clowdy":
+                        return_dic[arg] = f"a clowdiness of {weather_data['clouds']['all']} percent" #TODO: humidity verbal descriptors111
+                    if arg == "snowing":
+                        try:
+                            return_dic[arg] = f"up to {weather_data['snow']['1h']} milileters of rain"
+                            if "quantity" not in weather_arg:
+                                return_dic[arg] = "snow"
+                        except:
+                            return_dic[arg] = "no snow"
+                    
 
 
-                    return_string = f"{return_dic['default']} with {return_dic['rain']} {return_dic['quantity']} {return_dic['temp']} {return_dic['humidity']} {return_dic['clowdy']} {time_string} in {location}"
+            return_string = f"{return_dic['default']} {return_dic['connector']} {return_dic['rain']} {return_dic['quantity']} {return_dic['temp']} {return_dic['humidity']} {return_dic['clowdy']} {time_string} over at {weatherReport.location}"
                 
-                dispatcher.utter_message(f"{return_string}")
+            dispatcher.utter_message(f"{return_string}")
+        except KeyError:
+            dispatcher.utter_message("OPEN_WEATHER_MAP_API_KEY enviroment variable not set!")
 
         except Exception as e:
-            print(e)
-            dispatcher.utter_message(f"Could not connect to Open Weather Map services!")
+            dispatcher.utter_message(f"New Error\n{e}")
 
 class ActionReminderSet(Action):
 
@@ -145,9 +137,6 @@ class ActionReminderSet(Action):
 
     def run(self, dispatcher, tracker, domain):
         from custom_actions.action_reminder_set import CurrentReminders
-
-        trackers = tracker.current_slot_values()
-        print("trackers ", trackers)
         
         reminder = CurrentReminders()
         reminder_string = tracker.get_slot("reminder")
